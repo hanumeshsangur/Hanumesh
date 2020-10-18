6074128cad9ebeb8177feb9bc332f200d8bcfd6d
hanumeshsangur <72956477+hanumeshsangur@users.noreply.github.com>
2020-10-18 20:08:12 +0530

    Third Commit

---
+++ Zoo.java
@@ -0,0 +1,30 @@
+class Animals {
+	
+	public static void main(String [] params)
+	{
+		System.out.println("Well come to zoo");
+	}
+		public static void dogs(String [] params)
+		{
+			System.out.println("Dogs are domestic");
+		}
+		public static void cheetah(String [] params)
+		{
+			System.out.println("It is a fastest running animal");
+		}
+		
+			public static void Peacock(String [] params)
+		{
+			System.out.println("It is a national bird");
+		}
+		
+		public static void anaconda(String [] params)
+		{
+			System.out.println("It is a biggest snake");
+		}
+		
+			
+		
+			
+
+}
