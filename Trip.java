bd77f9dab031fa0e158ad627b816f98107443e68
hanumeshsangur <72956477+hanumeshsangur@users.noreply.github.com>
2020-10-18 20:13:33 +0530

    Fourth Commit

---
+++ Trip.java
@@ -0,0 +1,18 @@
+class BikeRiding{
+	public static void main(String [] params)
+	{
+		System.out.println("Going long ride in a bike");
+		
+		write();
+		write();
+		write();
+		
+		
+	}
+	
+	public static void write()
+	{
+		System.out.println("Riding a bike with a friends");
+	}
+
+}
