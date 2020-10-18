13c7fe3748b54cc36cf432bfe128a9f5414c5f51
hanumeshsangur <72956477+hanumeshsangur@users.noreply.github.com>
2020-10-18 20:14:56 +0530

    Fifth Commit

---
+++ Simple.java
@@ -0,0 +1,17 @@
+class DisplayName {
+	
+	public static void main(String [] params)
+	{
+		System.out.println("well come to x-workz");
+	}
+		public static void music(String [] params)
+		{
+			System.out.println("Play a music");
+		}
+		public static void drinking(String [] params)
+		{
+			System.out.println("I am  drinking water");
+		}
+			
+
+}
