f3768b5419662e2490c96910090d39fe91d1e699
hanumeshsangur <72956477+hanumeshsangur@users.noreply.github.com>
2020-10-20 21:26:05 +0530

    seventh commit

---
+++ Servers.java
@@ -0,0 +1,30 @@
+public class Servers{
+	public static void main(String[] args)
+	{
+		System.out.println("Measurements");
+		byte fingers;
+		fingers=10;
+		System.out.println(fingers);
+		short daysInYear;
+		daysInYear=365;
+		System.out.println(daysInYear);
+		int numberOfEngineersInIndia;
+		numberOfEngineersInIndia=50000;
+		System.out.println(numberOfEngineersInIndia);
+		long populationOfIndia;
+		populationOfIndia=423498745;
+		System.out.println(populationOfIndia);
+		float percentageOfStudent;
+		percentageOfStudent=9.6f;
+		System.out.println(percentageOfStudent);
+		double chargeOfElectron=32.12345d;
+		System.out.println(chargeOfElectron);
+		boolean earthIsFlat;
+		earthIsFlat=false;
+		System.out.println(earthIsFlat);
+		char grade;
+		grade='A';
+		System.out.println(grade);
+		
+	}
+}
