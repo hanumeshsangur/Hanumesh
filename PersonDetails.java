0699b686dd0346e05563c1694f0dc0b549b79313
hanumeshsangur <72956477+hanumeshsangur@users.noreply.github.com>
2020-10-20 21:27:56 +0530

    some program commit

---
+++ PersonDetails.java
@@ -0,0 +1,10 @@
+public class PersonDetails{
+	public static void main(String[] args){
+		final String aadharNumber="27635476364";
+		System.out.println("aadharNumber :"+ aadharNumber);
+		final String phoneNumber="8105784365";
+		System.out.println("phoneNumber :"+ phoneNumber);
+		final String email="hanu.smile@gmail.com";
+		System.out.println("email :"+ email);
+	}
+}
