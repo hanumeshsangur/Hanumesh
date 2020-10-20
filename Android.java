030e848dd0c6fa8a807dae4ecb86e389e8b4926e
hanumeshsangur <72956477+hanumeshsangur@users.noreply.github.com>
2020-10-20 21:23:11 +0530

    sixth commit

---
+++ Android.java
@@ -0,0 +1,30 @@
+public class Android{
+	public static void main(String[] args)
+	{
+		System.out.println("Total Numbers");
+		byte phoneNumbers;
+		phoneNumbers=10;
+		System.out.println(phoneNumbers);
+		short sundayInYear;
+		sundayInYear=60;
+		System.out.println(sundayInYear);
+		int numberOfPersonalLaptopsInBanglore;
+		numberOfPersonalLaptopsInBanglore=27354;
+		System.out.println(numberOfPersonalLaptopsInBanglore);
+		long productionOfMobilesInWorld;
+		productionOfMobilesInWorld=423483455;
+		System.out.println(productionOfMobilesInWorld);
+		float intrestInBank;
+		intrestInBank=7.4f;
+		System.out.println(productionOfMobilesInWorld);
+		double starsInGalaxy=32.15345d;
+		System.out.println(starsInGalaxy);
+		boolean allAreRichPersonsInWorld;
+		allAreRichPersonsInWorld=false;
+		System.out.println(allAreRichPersonsInWorld);
+		char grade;
+		grade='B';
+		System.out.println(grade);
+		
+	}
+}
